a8f449e87185958a516edf3cf5b3305e7af5227a
Prasad Joshi <pj9237084@gmail.com>
2026-02-04 08:41:25 +0530

    Fix: Enhance product filtering to support hierarchical category search and case-insensitivity

--- src/main/java/com/shop/repository/ProductRepository.java
+++ src/main/java/com/shop/repository/ProductRepository.java
@@ -11,33 +11,31 @@ import org.springframework.data.repository.query.Param;
 import com.shop.modal.Product;
 import com.shop.user.domain.ProductSubCategory;
 
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	@Query("SELECT p From Product p Where LOWER(p.category.name)=:category")
 	public List<Product> findByCategory(@Param("category") String category);
-	
+
 	@Query("SELECT p From Product p where LOWER(p.title) Like %:query% OR LOWER(p.description) Like %:query% OR LOWER(p.brand) LIKE %:query% OR LOWER(p.category.name) LIKE %:query%")
-	public List<Product> searchProduct(@Param("query")String query);
-	
+	public List<Product> searchProduct(@Param("query") String query);
 
-
-	
 	@Query("SELECT p FROM Product p " +
-	        "WHERE (p.category.name = :category OR :category = '') " +
-	        "AND ((:minPrice IS NULL AND :maxPrice IS NULL) OR (p.discountedPrice BETWEEN :minPrice AND :maxPrice)) " +
-		    "AND (:minDiscount IS NULL OR p.discountPersent >= :minDiscount) " +
-		    "ORDER BY " +
-		    "CASE WHEN :sort = 'price_low' THEN p.discountedPrice END ASC, " +
-		    "CASE WHEN :sort = 'price_high' THEN p.discountedPrice END DESC, "+
-		    "p.createdAt DESC")
+			"WHERE (:category = '' OR " +
+			"      LOWER(p.category.name) = LOWER(:category) OR " +
+			"      LOWER(p.category.parentCategory.name) = LOWER(:category) OR " +
+			"      LOWER(p.category.parentCategory.parentCategory.name) = LOWER(:category)) " +
+			"AND ((:minPrice IS NULL AND :maxPrice IS NULL) OR (p.discountedPrice BETWEEN :minPrice AND :maxPrice)) " +
+			"AND (:minDiscount IS NULL OR p.discountPersent >= :minDiscount) " +
+			"ORDER BY " +
+			"CASE WHEN :sort = 'price_low' THEN p.discountedPrice END ASC, " +
+			"CASE WHEN :sort = 'price_high' THEN p.discountedPrice END DESC, " +
+			"p.createdAt DESC")
 	List<Product> filterProducts(
-	        @Param("category") String category,
+			@Param("category") String category,
 			@Param("minPrice") Integer minPrice,
 			@Param("maxPrice") Integer maxPrice,
 			@Param("minDiscount") Integer minDiscount,
-			@Param("sort") String sort
-			);
-	
+			@Param("sort") String sort);
+
 	public List<Product> findTop10ByOrderByCreatedAtDesc();
 }
