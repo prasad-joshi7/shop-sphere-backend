e1b2068b0b8a87d3651de9b85d8dbb45664f1305
Prasad Joshi <pj9237084@gmail.com>
2026-02-04 08:24:13 +0530

    Cleanup: remove unused import

--- src/main/java/com/shop/config/JwtTokenValidator.java
+++ src/main/java/com/shop/config/JwtTokenValidator.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.crypto.SecretKey;
 
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
